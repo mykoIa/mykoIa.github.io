92367e02ffb4d2a35018f1b54b6be5ab4b468e9d
mykoIa <73664914+mykoIa@users.noreply.github.com>
2020-11-04 00:28:06 +0200

    Add files via upload

--- landing_page/script.js
+++ landing_page/script.js
@@ -188,3 +188,13 @@ function showSlidesReviews(n) {
     slides[slideIndexReviews - 1].style.display = "block";
     dots[slideIndexReviews - 1].className += " activeReviews";
 }
+
+// preloader
+
+window.onload = function() {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function() {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
